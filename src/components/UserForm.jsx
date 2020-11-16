015f3c375799e510868cd6ac35e527c6f071b4c2
Fallen Mireq <git.mireqfallen@gmail.com>
2020-11-16 17:20:29 +0500

    validation

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -38,9 +38,27 @@ export default function UserForm(props) {
         });
     }
 
+    const validation = {
+        name: !!name,
+        date: !!date,
+        email: !!email && /^.+@.+\..+$/.test(email),
+        phone: !!phone && phone.length === 12,
+        distance: !!distance,
+        payment: !!payment && /^\d+$/.test(payment),
+    };
+
+    const formValid = [
+        validation.name,
+        validation.date,
+        validation.email,
+        validation.phone,
+        validation.distance,
+        validation.payment,
+    ].every(x => x);
+
     return (
         <form className="user-form" onSubmit={(e) => submit(e)}>
-            <div className="field">
+            <div className={'field' + (!validation.name ? ' field_invalid' : '')}>
                 <label>ФИО</label>
                 <input
                     type="text"
@@ -49,39 +67,40 @@ export default function UserForm(props) {
                 />
             </div>
 
-            <div className="field">
+            <div className={'field' + (!validation.date ? ' field_invalid' : '')}>
                 <label>Дата рождения</label>
                 <DatePicker
                     value={date}
                     onInput={(value) => setDate(value)}
                 />
             </div>
-            <div className="field">
+            <div className={'field' + (!validation.email ? ' field_invalid' : '')}>
                 <label>Email</label>
                 <EmailInput
                     value={email}
                     onInput={(value) => setEmail(value)}
                 />
             </div>
-            <div className="field">
+            <div className={'field' + (!validation.phone ? ' field_invalid' : '')}>
                 <label>Телефон</label>
                 <PhoneInput
                     value={phone}
                     onInput={(value) => setPhone(value)}
                 />
             </div>
-            <div className="field">
+            <div className={'field' + (!validation.distance ? ' field_invalid' : '')}>
                 <label>Дистанция</label>
                 <select
                     value={distance}
                     onInput={(e) => setDistance(e.target.value)}
                 >
+                    <option value="" disabled>---</option>
                     <option>3</option>
                     <option>5</option>
                     <option>10</option>
                 </select>
             </div>
-            <div className="field">
+            <div className={'field' + (!validation.payment ? ' field_invalid' : '')}>
                 <label>Сумма взноса</label>
                 <input
                     type="number"
@@ -90,7 +109,7 @@ export default function UserForm(props) {
                 />
             </div>
             <div className="buttons">
-                <button type="submit">OK</button>
+                <button type="submit" disabled={!formValid}>OK</button>
                 <button type="button" onClick={() => clear()}>
                     Reset
                 </button>
